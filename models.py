746dd29170ae3699e74255793094a72c55b2db3e
ardesp <belloshehu1@gmail.com>
2020-03-05 11:55:41 +0100

    Project and Projectship models created and Tutor model was renamed to Author

--- models.py
+++ models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User, auth
 
 # Create your models here.
+TITLES =[
+    ('MR','Mr.'),
+    ('MRS','Mrs'),
+    ('ENGR','Engineer'),
+    ('DR','Doctor'),
+    ('PROF','Professor'),
+]
 '''class Shipping(models.Model):
     departure = models.CharField(verbose_name="Depart from")
 
@@ -12,7 +19,8 @@ class Products(models.Model):
     shipping = models.ForeignKey(Shipping,verbose_name="Shipping",on_delete=models.CASCADE)
 '''
 
-class Tutors(models.Model):
+class Author(models.Model):
+    title = models.Model(max_length=15, choices=TITLES)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     picture = models.FileField(verbose_name="Tutor's Picture")
     about = models.TextField(verbose_name='About Tutor')
@@ -22,21 +30,41 @@ class Tutors(models.Model):
         return '{} {}'.format(self.user.first_name, self.user.last_name)
 
 
-class Tutorials(models.Model):
+class Tutorial(models.Model):
     title = models.CharField(verbose_name='Title', max_length=100)
     content = models.TextField(verbose_name='Content', max_length=2000)
     video_link = models.URLField(verbose_name='Tutiral video link',max_length=50)
     images = models.FileField(verbose_name='Tutorial images')
-    tutor = models.ManyToManyField(Tutors, verbose_name="Tutor(s)",through='Tutorships')
+    tutor = models.ManyToManyField(Author, verbose_name="Tutor(s)",through='Tutorships')
+    date_published = models.DateTimeField()
 
     def __str__(self):
         return self.title
 
 
-class Tutorships(models.Model):
-    tutorial = models.ForeignKey(Tutorials, on_delete=models.CASCADE)
-    tutor = models.ForeignKey(Tutors, on_delete=models.CASCADE)
+class Tutorship(models.Model):
+    tutorial = models.ForeignKey(Tutorial, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
-        return '{} by {}, {}'.format(self.tutorial.title,self.tutor.user.first_name, self.tutor.user.last_name)
+        return '{} by {}, {}'.format(self.tutorial.title,self.author.user.first_name, self.author.user.last_name)
 
+class Project(models.Model):
+    title = models.CharField(verbose_name='Title', max_length=100)
+    description = models.TextField(verbose_name='Content', max_length=2000)
+    writeup = models.FileField(verbose_name='Project writeup')
+    cost = models.FloatField(verbose_name='Cost')
+    author = models.ForeignKey(Author, verbose_name='Author')
+    date_published = models.DateTimeField()
+    
+    def __str__(self):
+        return self.title
+
+
+class Projectship(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} by {}, {}'.format(self.project.title,self.author.user.first_name, self.author.user.last_name)
+    
