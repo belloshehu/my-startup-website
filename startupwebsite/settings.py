7dcd65b5682db3e00fc4e62aa835b73623264c0d
ardesp <belloshehu1@gmail.com>
2020-08-26 17:02:37 +0100

    Refactor code

--- startupwebsite/settings.py
+++ startupwebsite/settings.py
@@ -136,7 +136,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'startupwebsite/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
