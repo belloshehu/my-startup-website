1cb3885aaa681b72c5b738e4e9de3dc2bf126774
ardesp <belloshehu1@gmail.com>
2020-08-26 14:51:33 +0100

    Comment STATICFILESTORAGE

--- startupwebsite/settings.py
+++ startupwebsite/settings.py
@@ -130,7 +130,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATICFILES_DIRS = (
